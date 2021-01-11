2e58a4ca5c7b3554951adbc5c87164604ddcf88d
Ayman Aznadi <ayman.aznadi@gmail.com>
2021-01-11 15:40:24 +0100

    add fixes

--- theme/one-dark-pro.js
+++ theme/one-dark-pro.js
@@ -1,10 +1,33 @@
-// Fix for JS's `this` keyword
-// Run after prism.js is done
+// Figure out a way to run this after prism is done tokenizing the code
+// instead of using setTimeout
+
 setTimeout(() => {
-  const keywords = document.getElementsByClassName("keyword");
-  for (const keyword of keywords) {
-    if (keyword.innerText === "this") {
-      keyword.classList.add("this");
-    }
-  }
-}, 20);
+  // Fix for JS - `this` keyword
+  document
+    .querySelectorAll(".language-javascript .token.keyword")
+    .forEach((keyword) => {
+      if (keyword.innerText === "this") {
+        keyword.classList.add("this");
+      }
+    });
+
+  // Fix for Go - `:=` and `<-` operators
+  document
+    .querySelectorAll(".language-go .token.operator")
+    .forEach((operator) => {
+      if (operator.innerText === ":=") {
+        operator.classList.add("colon-equal");
+      } else if (operator.innerText === "<-") {
+        operator.classList.add("left-arrow");
+      }
+    });
+
+  // Fix for CSS - prefixes
+  document
+    .querySelectorAll(".language-css .token.property")
+    .forEach((property) => {
+      if (property.innerText.startsWith("-")) {
+        property.classList.add("prefix");
+      }
+    });
+}, 200);
